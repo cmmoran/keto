efe637c955dc6def189b1e7b61ab09e8f1b98ba0
Chris Moran <chris@chrismoran.com>
2024-01-06 04:52:33 -0500

    chore: renamed push, pop, peekCurrent -> pushContext, popContext, peekContext on parser chore: renamed checkArbitraryNamespaceHasRelation -> checkNamespacesHasRelationTypesRelations chore: renamed checkArbitraryRelationsTypesHaveRelation -> checkNamespacesHaveRelation feat: tupleToSubjectSetTraversalMaxDepth to manage recursion by traversal

--- internal/schema/limits.go
+++ internal/schema/limits.go
@@ -11,4 +11,8 @@ const (
 	// expressionNestingMaxDepth is the maximum number of nested '(' and '!' in
 	// a single 'permits'.
 	expressionNestingMaxDepth = 10
+
+	// tupleToSubjectSetTraversalMaxDepth controls the maximum depth of relation.traverse
+	// calls within a single namespace
+	tupleToSubjectSetTraversalMaxDepth = 10
 )
